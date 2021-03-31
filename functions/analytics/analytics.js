80847a3e9f0d9e3fdaf658706163f2a3e2d78468
Cleyson Reinhold <cleyson.reinhold@gmail.com>
2021-03-31 12:19:41 -0300

    removed semicolons

--- functions/analytics/analytics.js
+++ functions/analytics/analytics.js
@@ -1,25 +1,25 @@
-const { GoogleSpreadsheet } = require('google-spreadsheet');
-const { google_json, sheet_id } = process.env;
+const { GoogleSpreadsheet } = require('google-spreadsheet')
+const { google_json, sheet_id } = process.env
 	
 const handler = async (event) => {
   try {
-	const doc = new GoogleSpreadsheet(sheet_id);
-	await doc.useServiceAccountAuth(google_json);
-	await doc.loadInfo();
-	const sheet = doc.sheetsByIndex[0];
+	const doc = new GoogleSpreadsheet(sheet_id)
+	await doc.useServiceAccountAuth(google_json)
+	await doc.loadInfo()
+	const sheet = doc.sheetsByIndex[0]
     
     
-	const data = JSON.parse(event.body);
-	const data2 = JSON.stringify(event);
-	const addedRow = await sheet.addRow(data);
-	const addedRow2 = await sheet.addRow(data2);
+	const data = JSON.parse(event.body)
+	const data2 = JSON.stringify(event)
+	const addedRow = await sheet.addRow(data)
+	const addedRow2 = await sheet.addRow(data2)
 		
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Success!` });
-    };
+      body: JSON.stringify({ message: `Success!` })
+    }
   } catch (error) {
-    return { statusCode: 500, body: error.toString() };
+    return { statusCode: 500, body: error.toString() }
   }
 }
 
