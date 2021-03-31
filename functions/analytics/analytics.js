a29801d62cee3a04954f4affa71fa6254b143ac9
Cleyson Reinhold <cleyson.reinhold@gmail.com>
2021-03-31 14:37:35 -0300

    analytics update

--- functions/analytics/analytics.js
+++ functions/analytics/analytics.js
@@ -10,9 +10,10 @@ const handler = async (event) => {
     
     
 	await sheet.addRow({
-    referer: event.headers.referer,
-    country: event.multiValueHeaders['X-Country'],
-    agent: event.headers['user-agent'],
+    referer: event?.headers?.referer || '',
+    country: event?.multiValueHeaders?['X-Country'] || '' : '',
+    agent: event?.headers?['user-agent'] || '' : '',
+    date: new Date().toISOString(),
   })
 		
     return {
